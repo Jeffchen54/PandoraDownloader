c79bd493ad96ab7cc56c598830b7539f2c5943b4
jeff chen <chenjeff4840@vt.edu>
2022-08-29 12:15:51 -0400

    Finished skeleton for Services.py, added part 1 files

---
+++ Services.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Services(Enum):
+    """
+    Services supported by PandoraDownloader
+    Each member value is associated with its class instance name
+    For example, Services.Kemono = KemonoComponent.
+    # TODO add component
+    """
+    KEMONO = None
+    
